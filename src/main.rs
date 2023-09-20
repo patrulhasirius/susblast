d2c925c19306d226548144406c590c629c070bb4
lucas <13894426+patrulhasirius@users.noreply.github.com>
2023-09-20 20:52:40 -0300

    clippy

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,5 @@ use crate::scripts::get_files;
 
 fn main() {
     println!("Hello, world!");
-    let file = get_files(vec![1], vec![1]);
-    println!("{:#?}", file);
+    println!("{:#?}", get_files(vec![1], vec![1]));
 }
--- src/scripts/decompress.rs
+++ src/scripts/decompress.rs
@@ -1,7 +1,4 @@
-use explode;
-
-pub fn decompress(file: &Vec<u8>) -> Vec<u8> {
+pub fn decompress(file: &[u8]) -> Vec<u8> {
     //TODO deal with header weirness
-    let result = explode::explode(file).expect("error in file exploding");
-    result
+    explode::explode(file).expect("error in file exploding")
 }
