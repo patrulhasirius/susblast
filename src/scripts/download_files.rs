028da4d8ccbacc8bfabb25c3db118fbc8ec082ab
lucas <13894426+patrulhasirius@users.noreply.github.com>
2023-09-20 20:11:55 -0300

    cargo fix

--- src/scripts/download_files.rs
+++ src/scripts/download_files.rs
@@ -1,6 +1,6 @@
 use suppaftp::FtpStream;
 
-pub fn download_files(dates: Vec<String>) -> Vec<Vec<u8>> {
+pub fn download_files(_dates: Vec<String>) -> Vec<Vec<u8>> {
     let path = String::from("/dissemin/publicos/SIASUS/200801_/Dados/");
     let mut ftp_stream =
         FtpStream::connect("ftp.datasus.gov.br:21").unwrap_or_else(|err| panic!("{}", err));
@@ -11,7 +11,7 @@ pub fn download_files(dates: Vec<String>) -> Vec<Vec<u8>> {
         .expect("erro no login");
     ftp_stream.cwd(path).expect("error changing folder");
     let mut files: Vec<Vec<u8>> = vec![];
-    dates.iter().for_each(|x| {
+    dates.iter().for_each(|_x| {
         files.push(
             ftp_stream
                 .retr_as_buffer("PARS2307.dbc")
