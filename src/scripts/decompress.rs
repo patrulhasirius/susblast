0374d456f86308504b51390c7b1df77ee6d3d462
Doguinhoo <leo.ribeiropoa@gmail.com>
2023-09-21 01:43:11 -0300

    Cria a função de descomprimir o conteúdo dos arquivos dbc
    Now let's see how well you fare against my BIDEN BLAST!!!

--- src/scripts/decompress.rs
+++ src/scripts/decompress.rs
@@ -1,4 +1,60 @@
-pub fn decompress(file: &[u8]) -> Vec<u8> {
+/*
+* Now let's see how well you fare against my BIDEN BLAST!!!
+xxxoloxxxxk0XK00KK0OxlcloxkkkkkkkOOOOOkkkkkkkxddxxxkOOOOOOOOOOOOkkkkkkkOOxdc''lk
+xxdlldxddxOKK0kkO00OxollldkOOkkkkkkOOOkkkkkkkxdoodxxkOkkOOOOO0000OOOkkxddooc''lk
+xdoloddldxOX0kdlok0Oxolc::okO0OxdddkOkkkkkkxxdddddxxxxkkkOOOOOO00OOOOkxdolc:;;lk
+kxdoddoodxOK0xo:;cxOxlc:;,,lkOxoccoxkkxxxxxddddxxxdlcoxdodxkOOOOOOOOOOkkxdl:;:lx
+kkkxddoddk0KOxl:'':ddc:;;'':dxdl::cxOkxkkkkxxxxxdc;:clldxOOO0000000OOOOkkxoc,,:o
+OOOkdddxxk0KOxl:'.'ldoccc:;;oxdlc;,lkOOO0OOOkxxxc,:ccodxkkOO0000000000Okxolc:',c
+OO0OxdxkkO0KOdc,'.'lkkxxxdc;:ddl;'',lO000OOOOkkd;,cc:oddxkkO000KKKK000OOkdodc'.,
+OO00OOO00KKKOdc;,''ckOkkxdc,,col;...,d0000OOOkkxc,;:;coddxkOOO000000000Okxddl:;,
+O0KKKKKKXXXKOxo:;'.;xOOkxdc,'':l;...'ck0000000O0d,';;;coddxkOOOOOOO000Okkkxdlll;
+0KKKKXXXKKXXK0kdl:',oOOkxdl:'.,c:,'',;d0KK00000KOc.,;;codddxkkkxddxxdlcloool:::,
+KKKKKXXKOk0NNK0kdl,':xkxddoc,.'cdoc;::cx000000000d,'',:oooddol:;,,oxl;,;;:;;:::,
+KKKKXXKOxokXNXKOko:''lxddddc,'.;dxl::::okO0000000kc'.';clc:;''',:cdOxooooxddddoc
+XXXXXXKkdco0NNKOkdc,';dxdxxl,'.'cdl:;;;cldkOOOOO0kl,',;cooc;:ccoxxxkkdoxOOOOkxoc
+XXXXXX0ko::kNX0Okxl;';dOOOOdc,'.,ll:;:c::lxkkkOO0x:,,;:cldddkkOOOxodkkocdOOOkdl:
+XXXXXK0xl;;dXXOkkxo:,;xKXXKOd:,.':lc:llccldkOOOOOkdc,..,coodkO00xoloxkl;:ldxdl;'
+000KXK0xl:;l0XK00kdc:;oKNNKOxl;,':dxxkkdooddooollllc;'..':lldxxolc:;,'...:ddol;'
+:cclOXKkdc:l0NNXX0kxoclONNXKkdc;,lOKKXX0Oxoc,......,cc'...;:clllxxoc:;,;:lddddl,
+;;;cONNKOxooONWNNKOkdccxXNXK0xo:;o0NNWWX0kdl:,,''''.,c:'...,;:cldddddllc:;,,:odc
+xxxkKNNX0OxoxXWWNNXK0OkOXNXK0koc:lONWWWNXOdllddddddolodo;...',::,'''...'.',:ldxl
+KKKKXNNXK0OO0NMMMMMMWMMMWWWWNKkoclONWWWNX0xookKXKKKKK00Okl...':l:;,,;:cccllodoo:
+XXXXNNNNNNWWMMMMMMMMMMMMMMMMMMWXKKNWWWWNX0xold0XXXXXXKKKK0l...,clllcc:;;cloddc;.
+NNNNNNWWMMMMMMMMMMMMMMMMMMMMMMMMMMMMWWWNNKkdookKNXXKK000OOl. ...,:clllodxxddo:..
+XXXXNWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWWNN0kxdxOKK000OOOx:.     ..,coooc:,,;,.  
+KKXNWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWWNX0Oxddk000koc:'     ..  ..'''..  ..  .
+KKNWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWWNXXKOxodOOl'...        ...............l
+KXWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWWWNNXKOdoxx:...        .':lddddoooolllld
+XWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWWNXKK0kdlokx;...     .,lkOK00OOkxxddolc;
+XWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWXO0KX0kdodOo....  .;oO0KKK0Okxolc:;'...
+NWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWNNWNKkdloxx:....,oOKXXK00kxoc;'.......
+XWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWNXOdlcldko'..lOKKXKK0kxoc;'.   ..;c,
+XNWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWWNX0dc;;:odl:xKXXXK0Oxoc;..       .lo
+KXWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWWNXOdlc:::oOKXNXK0Okdl;..          .;
+kOXWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWWNNKkoc::lx0XXXXKOxdo:'..            .
+ddkXWWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWWWWNKOdoodxOKXXXKOxoc;..               
+kkk0XWWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWWWWWNX0kxddxOKXXK0Oxl;...                
+O000OOKNMMMMMMMMMMMMMMMMMMMMMMMMMMMMWWWWWWNNK0OOkO0KKKKKOxl:..                  
+XKKOo:cdXWMMMMMMMMMMMMMMMMMMMMMMMWWWWWWWWWNNNNNXKKK0OOOkdc,...               .  
+K0Okxoc:xXWWWWWMMMMMMMMMMMMMMMMMWWWWWWWWWWNNNNXK00Oxol:,..                     .
+dolcllc;:kXNWNNWWWNNNNNNNNWWWWWWWWWWWWWWWWNXK0Oxdol:'.                          
+::;,;;:;;l0NNNNNXXKK0000KXNNWWWWWWWWWWWNNNK0Oxoc:;'..
+*/
+
+pub fn decompress(file: &Vec<u8>) -> Vec<u8> {
+    let start: usize = (file[8] as usize) + ((file[9] as usize) << 8) + 4;
+
+    let mut header = file[..(start - 4)].to_vec();
+    let content = file[start..].to_vec();
+
     //TODO deal with header weirness
-    explode::explode(file).expect("error in file exploding")
+    let mut decompressed = explode::explode(&content).expect("error in file exploding");
+
+    let mut result: Vec<u8> = vec![];
+
+    result.append(&mut header);
+    result.append(&mut decompressed);
+
+    return result;
 }
--- src/scripts/interpret_file.rs
+++ src/scripts/interpret_file.rs
@@ -4,5 +4,5 @@ pub fn interpret_file(file: Vec<u8>) -> Vec<dbase::Record> {
     let mut saved_file = File::create("./test_files/test.dbf").unwrap();
     saved_file.write_all(&file).expect("problem creeating file");
 
-    dbase::read("tests/data/line.dbf").expect("problem reading file")
+    dbase::read("./test_files/test.dbf").expect("problem reading file")
 }
