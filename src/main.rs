20b95c5602b0d8c7852a769e616d5de659e3b0b3
Doguinhoo <leo.ribeiropoa@gmail.com>
2023-09-21 02:07:50 -0300

    :)

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,6 @@ mod scripts;
 use crate::scripts::get_files;
 
 fn main() {
-    println!("Hello, world!");
+    println!("Now let's see how well you fare against my BIDEN BLAST!!!");
     println!("{:#?}", get_files(vec![1], vec![1]));
 }
